38a95f449f1f20121c61577abec81e568b20937a
zwf <871772825@qq.com>
2023-01-30 14:43:30 +0800

    feat： 2023/1/30

---
+++ 202301/code/vue3-defense-xss/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+// eslint-disable-next-line no-useless-escape
+const xssText = ref(`<script> console.log( 'cookie数据为',  document.cookie ) <\/script>`)
+
+// alert(document.cookie)
+</script>
+
+<template>
+  <!-- 表达式无害,因为会通过createTextNode处理成纯字符串 -->
+  <div>
+    {{ xssText }} 
+  </div>
+  <!-- 使用v-html有害,会执行脚本文件 -->
+  <div v-html="xssText">
+  </div>
+</template>
+
+<style scoped lang="scss">
+</style>
